8fa25b9af84bdcd007a21bf35d0fae3bcc5cb70f
mattix10 <mateusz.salamon10@gmail.com>
2021-11-22 10:09:41 +0100

    remove unuseful code

--- src/app/dashboard/main-device/main-device.component.ts
+++ src/app/dashboard/main-device/main-device.component.ts
@@ -1,41 +1,15 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ChartType } from 'src/app/core/enums/chartType';
-import { SocketService } from 'src/app/core/services/socket.service';
 @Component({
   selector: 'app-main-device',
   templateUrl: './main-device.component.html',
   styleUrls: ['./main-device.component.scss']
 })
-export class MainDeviceComponent implements OnInit, OnChanges {
+export class MainDeviceComponent {
 
-  @Input() currentValue: number | null = null;
-
+  @Input() currentValue: any = null;
   chartType: ChartType = ChartType.Bar;
-  myOptions = {
-    colors: ['#0D77B4', ],
-    legend: { position: 'none' },
-    hAxis: {
-      title: 'Godzina',
-      titleTextStyle: {
-        italic: false,
-      }
-    },
-    vAxis: {
-      title: '',
-      titleTextStyle: {
-        italic: false,
-      }
-    }
-  };
-  myData = [
-    ['Wypełnienie zbiornika', 71],
-  ];
-  constructor(private socketService: SocketService) { }
 
-  ngOnInit(): void {
-    // this.socketService.listen()
-  }
+  constructor() { }
 
-  ngOnChanges(changes: SimpleChanges) {
-  }
 }
