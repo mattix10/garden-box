ec5b880ed121375f5411ad42506c8bad3cc04cb6
mattix10 <mateusz.salamon10@gmail.com>
2021-11-23 12:16:05 +0100

    add edit password feature

--- src/app/dashboard/my-account/my-account.component.ts
+++ src/app/dashboard/my-account/my-account.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { MyAccountService } from 'src/app/core/services/my-account.service';
 import jwt_decode from "jwt-decode";
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-my-account',
@@ -10,14 +11,25 @@ import jwt_decode from "jwt-decode";
 })
 export class MyAccountComponent implements OnInit {
 
+  newPassword: FormControl = new FormControl('', [Validators.minLength(6)]);
+  email: string = '';
+  editMode: boolean = false;
+
   constructor(private myAccountService: MyAccountService, private authService: AuthService) { }
 
-  email: any;
   ngOnInit(): void {
+    const decoded: any = jwt_decode(localStorage.token);
+    this.email = decoded?.email;
+  }
 
-  const decoded: any = jwt_decode(localStorage.token);
-  this.email = decoded?.subject;
-    console.log(decoded)
+  editPassword() {
+    this.editMode = true;
+  }
+
+  save() {
+    console.log(this.newPassword)
+    this.editMode = false;
+    this.myAccountService.editPassword(this.email, this.newPassword.value)
   }
 
   logout() {
