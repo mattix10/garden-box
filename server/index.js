3adab353db49f05e967bcbaa34d1ed0c3388d5d0
mattix10 <mateusz.salamon10@gmail.com>
2021-11-24 15:52:03 +0100

    remove unused code

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const temperature = require('./parameters/temperature');
 const container = require('./parameters/container');
 const sequelize = require('./database');
 const router = require('./routes');
-const INTERVAL = 5000;
+const INTERVAL = 5 * 1000; // godziny * minuty * sekundy * milisekundy
 const path = require('path');
 const moment = require('moment');
 const session = require("express-session")({
@@ -15,6 +15,8 @@ const session = require("express-session")({
   resave: true,
   saveUninitialized: true
 });
+
+
 const sharedsession = require("express-socket.io-session");
 const Sensor = require('./models/Sensor');
 
@@ -22,33 +24,27 @@ sequelize.sync().then(() => console.log('db is ready'));
 
 const app = express();
 
+// const expressServer = app.listen(80, '192.168.137.160', async () => {
 const expressServer = app.listen('5000', async () => {
   console.log('listen');
 
-  // Sensor.drop();
+  setInterval(async () => {
+    let now = new moment();
+    let lightValue = light.getLight();
+    let temperatureValue = temperature.getTemperature();
+    let humidityValue = humidity.getHumidity();
+    let containerValue = container.getContainer();
+    let airValue = air.getAir();
 
-  // setInterval(async () => {
-  //   let now = new moment();
-  //   let lightValue = light.getLight();
-  //   let temperatureValue = temperature.getTemperature();
-  //   let humidityValue = humidity.getHumidity();
-  //   let containerValue = container.getContainer();
-  //   let airValue = air.getAir();
-  //   console.log(lightValue);
-  //   console.log(temperatureValue);
-  //   console.log(humidityValue);
-  //   console.log(containerValue);
-  //   console.log(airValue);
-
-  //   await Sensor.create({
-  //     light: lightValue,
-  //     temperature: temperatureValue,
-  //     humidity: humidityValue,
-  //     container: containerValue,
-  //     air: airValue,
-  //     createdAt: now.format("YYYY-MM-DD HH:mm:ss")
-  //   })
-  // }, INTERVAL);
+    await Sensor.create({
+      light: lightValue,
+      temperature: temperatureValue,
+      humidity: humidityValue,
+      container: containerValue,
+      air: airValue,
+      createdAt: now.format("YYYY-MM-DD HH:mm:ss")
+    })
+  }, INTERVAL);
 
 });
 
@@ -56,20 +52,23 @@ const expressServer = app.listen('5000', async () => {
 app.use(cors({
   origin: '*'
 }))
-
-app.use(express.static('dist/garden-box'))
-app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+app.use(express.static('dist/garden-box'));
 app.use('/api', router);
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../../server/dist/garden-box/index.html'))
+  res.sendFile(path.join(__dirname + '../../server/dist/garden-box/index.html'));
 })
 
 // express-session middleware for express
 app.use(session);
 const io = require("socket.io")(expressServer, {
   cors: {
+    // origin: "192.168.137.160:80",
     origin: "*",
-    methods: ["GET", "POST"]
+    methods: ["GET", "POST", "PATCH"]
   }
 });
 // shared session middleware for socket.io
