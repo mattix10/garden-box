b7710ce86f050d80b0ff35c0da0e0956c3335092
mattix10 <mateusz.salamon10@gmail.com>
2021-11-25 10:30:13 +0100

    type function arguments

--- src/app/dashboard/main-table/main-table.component.ts
+++ src/app/dashboard/main-table/main-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Measurement } from 'src/app/core/interfaces/Measurement';
 import { MenuItem } from 'src/app/core/interfaces/MenuItem';
 import { MeasurementsService } from 'src/app/core/services/measurements.service';
 import { MenuItemsService } from 'src/app/core/services/menu-items.service';
@@ -12,21 +13,25 @@ import { MenuItemsService } from 'src/app/core/services/menu-items.service';
 export class MainTableComponent implements OnInit {
   selectedParameter: MenuItem | any;
   parameters: MenuItem[] = [];
-  data: any[] = [];
-  empty: boolean = false;
+  data: Measurement[] = [];
 
-  constructor(private measurementsService: MeasurementsService, private menuItemsService: MenuItemsService, private router: Router) { }
+  constructor(
+    private measurementsService: MeasurementsService, 
+    private menuItemsService: MenuItemsService, 
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    this.parameters = this.menuItemsService.getMenuItems().filter((item: any )=> item.category == 'parameters');
+    this.parameters = this.menuItemsService.getMenuItems()
+      .filter((item: MenuItem)=> item.category == 'parameters');
     this.selectedParameter = this.parameters[0];
     this.getMeasurement(this.selectedParameter);
-    if(this.router.url == '/panel/statystyki') {
+    if(this.router.url === '/panel/statystyki') {
       this.getDateMeasurement();
     }
   }
 
-  onSelectedParam(param: any) {
+  onSelectedParam(param: MenuItem) {
     this.selectedParameter = param;
     if(this.router.url == '/panel/statystyki') {
       this.getDateMeasurement();
@@ -37,15 +42,15 @@ export class MainTableComponent implements OnInit {
     this.measurementsService.dateSubject.subscribe((choosenDate: string) => {
       this.measurementsService.getDateMeasurement(this.selectedParameter.name, choosenDate).subscribe(data => {
         this.data = data;
-        if(!this.data.length) this.empty = true;
+        // if(!this.data.length) this.empty = true;
       });
     })
   }
 
-  getMeasurement(param: any, limit: number = 10) {
+  getMeasurement(param: MenuItem, limit: number = 10) {
     this.measurementsService.getMeasurement(param.name, limit).subscribe(data => {
       this.data = data;
-      if(!this.data.length) this.empty = true;
+      // if(!this.data.length) this.empty = true;
     });
   }
 
