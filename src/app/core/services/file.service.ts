bec3362f9e37c86cd761092d8502e4fbce12c6d3
mattix10 <mateusz.salamon10@gmail.com>
2021-11-27 23:03:18 +0100

    add getImages()

--- src/app/core/services/file.service.ts
+++ src/app/core/services/file.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -9,7 +10,12 @@ export class FileService {
 
   constructor(private http: HttpClient) { }
 
-  upload(formData: FormData) {
-    return this.http.post(`${environment.API_URL}/upload`, formData);
+  upload(formData: FormData): Observable<any> {
+    console.log(`${environment.API_URL}/upload`)
+    return this.http.post<any>(`${environment.API_URL}/upload`, formData);
+  }
+
+  getImages(): Observable<any> {
+    return this.http.get<any>(`${environment.API_URL}/user/images`)
   }
 }
