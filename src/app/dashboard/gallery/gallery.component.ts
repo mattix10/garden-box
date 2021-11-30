6e1fe3decc4c815d7e4058dae19f6014586d2114
mattix10 <mateusz.salamon10@gmail.com>
2021-11-30 08:24:23 +0100

    upload and get images in gallery

--- src/app/dashboard/gallery/gallery.component.ts
+++ src/app/dashboard/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
 import { FileService } from 'src/app/core/services/file.service';
 
 @Component({
@@ -10,23 +11,30 @@ export class GalleryComponent implements OnInit {
 
   fileName: string = '';
   constructor(private fileService: FileService) { }
-
+  images$: Observable<any>;
+  images: any[] = [];
+  @ViewChild('fileUpload') fileUpload: any;
 
   ngOnInit(): void {
+    // this.images$ = this.fileService.getImages().pipe(startsWith([]))
+    this.fileService.getImages().subscribe(data => {
+      console.log(data)
+      this.images = data.images
+    })
   }
 
   onSelectFile(event: any) {
-
+    event.preventDefault();
     const file:File = event.target.files[0];
 
     if (file) {
         this.fileName = file.name;
         const formData = new FormData();
-        formData.append("thumbnail", file);
+        formData.append("image", file);
 
-        this.fileService.upload(formData);
+        this.fileService.upload(formData).subscribe(data => console.log(data));
     }
 }
 
-
 }
+
