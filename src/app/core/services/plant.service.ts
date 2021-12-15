44f8aa9f3c4c2895c7a00f25ce64e24ee24b5657
mattix10 <mateusz.salamon10@gmail.com>
2021-12-15 23:01:07 +0100

    add plant service

---
+++ src/app/core/services/plant.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const API_URL = environment.API_URL;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlantService {
+
+  constructor(private http: HttpClient) { }
+
+  editPlant(name: string): Observable<any> {
+    console.log('here')
+    return this.http.patch(`${API_URL}/plant`, {name});
+  }
+
+  getPlant(): Observable<any> {
+    return this.http.get(`${API_URL}/plant`);
+  }
+}
