44fb679d54f34e7dba3c3e829ca398ad9467f18c
mattix10 <mateusz.salamon10@gmail.com>
2021-12-21 20:14:46 +0100

    add getPlant and editPlant methods, display edit icon

--- src/app/dashboard/main-info/main-info.component.ts
+++ src/app/dashboard/main-info/main-info.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PlantService } from 'src/app/core/services/plant.service';
 
 @Component({
   selector: 'app-main-info',
@@ -9,12 +12,34 @@ export class MainInfoComponent implements OnInit {
 
   date: string = new Date().toLocaleDateString('PL-pl');
   time: string = new Date().toLocaleTimeString('Pl-pl');
-  constructor() { }
+  showEditIcon: boolean = false;
+  editMode: boolean = false;
+  plantName: FormControl = new FormControl('');
+  plantname: string = '';
+  constructor(private router: Router, private plantService: PlantService) { }
 
   ngOnInit(): void {
+    if(this.router.url ==='/panel/informacje') this.showEditIcon = true;
+    else this.showEditIcon = false;
     setInterval(()=> {
       this.time = new Date().toLocaleTimeString('PL-pl');
     }, 1000)
+    this.getPlant();
+  }
+
+  getPlant(): void {
+    this.plantService.getPlant().subscribe((data: any) =>{
+     this.plantname = data.plant.name
+    })
+  }
+
+  editPlant(): void {
+    this.showEditIcon = true;
+    this.editMode = false;
+    this.plantService.editPlant(this.plantName.value).subscribe((data:any) => {
+      this.getPlant();
+      console.log(data)
+    })
   }
 
 }
