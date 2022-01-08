006e0540e7d0a07d898b226ae55da5c6f8c94735
mattix10 <mateusz.salamon10@gmail.com>
2022-01-08 13:13:35 +0100

    add behaviorSubject and change navigation url

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { User } from '../interfaces/User';
@@ -15,23 +15,23 @@ export class AuthService {
 
   constructor(private http: HttpClient, private router: Router) { }
   user: string = '';
+  registrationInfo$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   login(email: string, password: string): Observable<User> {
-    return this.http.post<User>(`${API_URL}/zaloguj`, {email, password})
+    return this.http.post<User>(`${API_URL}/login`, {email, password})
     .pipe(
       tap((data: User) => {
-      console.log(data)
       this.user = data.email
     }
     ));
   }
 
-  registration(email: string, password: string): Observable<User>  {
+  registration(email: string, password: string): Observable<any>  {
     return this.http.post<User>(`${API_URL}/registration`, {email, password});
   }
 
   logout(): void {
     window.localStorage.clear();
-    this.router.navigateByUrl('/zaloguj');
+    this.router.navigateByUrl('/logout');
   }
 }
