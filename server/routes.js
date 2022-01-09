991e8615945411dab8c78e07e13e4eb0a81f798a
mattix10 <mateusz.salamon10@gmail.com>
2022-01-09 18:33:44 +0100

    rename endpoints

--- server/routes.js
+++ server/routes.js
@@ -1,38 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const Measurement = require('./models/Measurement');
-const sequelize = require('sequelize');
 const auth = require('./controllers/auth.controller');
-const {
-  Op
-} = require("sequelize");
 const user = require('./controllers/user.controller');
-const url = require('url');
 const fileUpload = require('express-fileupload');
-const plant = require('./plant');
+const plant = require('./controllers/plant.controller');
 const images = require('./controllers/images.controller');
 const parameter = require('./controllers/parameter.controller');
 
 router.use(fileUpload());
+
+router.post('/plant/images', images.uploadImage);
 router.get('/plant/images', images.getImages);
-router.delete('/plant/images/:createdAt', images.removeImage);
-router.put('/user', user.resetPassword); // zmienić na liczbe mnogą 'users'
+router.delete('/plant/images/:name', images.removeImage);
 
 router.get('/plant', plant.getPlant);
 router.patch('/plant', plant.editPlant);
 
+router.put('/user', user.resetPassword);
+
 router.post('/registration', auth.registration);
-router.post('/zaloguj', auth.login);
+router.post('/login', auth.login);
 
 router.get('/:parameter/oneDay?', parameter.getOneDayParameter)
-
-router.get('/:parameter/:limit', );
-
+router.get('/:parameter/:limit', parameter.getParameter);
 router.get('/:parameter?', parameter.getDateParameter);
-
-router.get('/sensors', async (req, res) => {
-  const sensors = await Measurement.findAll();
-  res.status(200).json(sensors);
-});
 
 module.exports = router;
