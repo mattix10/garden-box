6335d9a6dc0a798595af35e3936334cb1dfaee66
mattix10 <mateusz.salamon10@gmail.com>
2022-01-12 23:04:34 +0100

    read and write humidityValue to file

--- server/parameters/humidity.js
+++ server/parameters/humidity.js
@@ -1,20 +1,28 @@
-const Sensor = require('../models/Sensor');
 let humidityValue = 65;
+const fs = require('fs');
 
 exports.socketHumidity = function (socket) {
   socket.on('humidity', (value) => {
+    fs.readFile('./setValues/outputHumidity.txt', 'utf8', function (err, data) {
+      if (err) {
+        console.log(err);
+        return
+      }
+      humidityValue = data;
+    });
     socket.emit('humidity', {
-      date: new Date().toLocaleTimeString(),
-      value: humidityValue + 1,
+      createdAt: new Date().toLocaleTimeString(),
+      value: +humidityValue,
       name: 'humidity'
     });
   })
-}
 
-exports.getHumidity = () => humidityValue + Math.floor(Math.random() * 7);
-// exports.getHumidity = async () => {
-//   await Sensor.create({
-//     value: humidityValue + Math.floor(Math.random() * 7),
-//     name: 'humidity'
-//   });
-// }
+  socket.on('sethumidity', value => {
+    fs.writeFile('./setValues/humidity.txt', `${value}`, err => {
+      if (err) {
+        console.error(err);
+        return
+      }
+    });
+  })
+}
