819257807f4f429a7dfa9dae8c61718d268ee95a
mattix10 <mateusz.salamon10@gmail.com>
2022-01-24 23:17:32 +0100

    remove unused import

--- src/app/core/guards/login.guard.ts
+++ src/app/core/guards/login.guard.ts
@@ -1,19 +1,14 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, NavigationEnd, Router, RouterStateSnapshot, RoutesRecognized, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { CanActivate, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuard implements CanActivate {
   constructor(private router: Router) {}
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate(): boolean {
       const token = window.localStorage.getItem('token');
       if(token) {
-        console.log(route);
         this.router.navigateByUrl('/panel')
         return false;
       } else return true;
