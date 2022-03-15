e05bbf301a08974bb457cf65e6fd05715931068d
mattix10 <mateusz.salamon10@gmail.com>
2022-03-15 23:01:15 +0100

    add chartService tests

--- src/app/core/services/chart.service.spec.ts
+++ src/app/core/services/chart.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Measurement } from '../interfaces/Measurement';
+import { DataChart } from '../interfaces/DataChart';
 import { ChartService } from './chart.service';
 
 describe('ChartService', () => {
@@ -13,4 +14,40 @@ describe('ChartService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should create data for chart', () => {
+    const measurements: Measurement[] = [
+      {value: 24, createdAt: '2021-09-09 20:00:05'},
+      {value: 44, createdAt: '2021-02-19 19:11:05'},
+    ]
+
+    const dataChart: DataChart= [
+      ['20:00', 24],
+      ['19:11', 44]
+    ]
+    expect(service.createDataForChart(measurements)).toEqual(dataChart)
+  })
+
+  it('should return chartOptions', () => {
+    const chartOptions = {
+      colors: ['#38AA73'],
+      legend: {
+        position: 'none' 
+      },
+      hAxis: {
+        title: 'Godzina',
+        titleTextStyle: {
+          italic: false,
+        }
+      },
+      vAxis: {
+        title: '',
+        titleTextStyle: {
+          italic: false,
+        }
+      }
+    };
+
+    expect(service.getChartOptions()).toEqual(chartOptions);
+  })
 });
