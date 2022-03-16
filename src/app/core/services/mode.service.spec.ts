df9daeb1bc7593c9a05a3792eb9cf57ef13c9a68
mattix10 <mateusz.salamon10@gmail.com>
2022-03-16 22:45:06 +0100

    add modeService tests

--- src/app/core/services/mode.service.spec.ts
+++ src/app/core/services/mode.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { ModeService } from './mode.service';
 
-describe('ModeService', () => {
+fdescribe('ModeService', () => {
   let service: ModeService;
 
   beforeEach(() => {
@@ -13,4 +13,24 @@ describe('ModeService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it(`should set temperature's mode`, () => {
+    service.setMode('temperature', true)
+    expect(service.getMode('temperature')).toBeTrue();
+  })
+
+  it(`should set light's mode`, () => {
+    service.setMode('light', true)
+    expect(service.getMode('light')).toBeTrue();
+  })
+
+  it(`should set humidity's mode`, () => {
+    service.setMode('humidity', true)
+    expect(service.getMode('humidity')).toBeTrue();
+  })
+
+  it('should return default value', () => {
+    service.setMode('any', true);
+    expect(service.getMode('any')).toBeFalse();
+  })
 });
