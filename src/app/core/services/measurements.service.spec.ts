4e58b0303e2275655228420a65dd6a14d4888d84
mattix10 <mateusz.salamon10@gmail.com>
2022-03-17 09:18:09 +0100

    add measurementService tests

--- src/app/core/services/measurements.service.spec.ts
+++ src/app/core/services/measurements.service.spec.ts
@@ -1,16 +1,101 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { fakeAsync, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Measurement } from '../interfaces/Measurement';
 
 import { MeasurementsService } from './measurements.service';
 
-describe('MeasurementsService', () => {
-  let service: MeasurementsService;
+fdescribe('MeasurementsService', () => {
+  let measurementsService: MeasurementsService;
+  let httpTestingController: HttpTestingController;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  const testUrl = `${environment.API_URL}`;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MeasurementsService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['post', 'get', 'delete']);
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, HttpClientTestingModule],
+      providers: [
+        MeasurementsService
+      ]
+    });
+    measurementsService = TestBed.inject(MeasurementsService);
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
+  afterEach(() => {
+    httpTestingController.verify();
+  })
+
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(measurementsService).toBeTruthy();
+  });
+
+  it('should get 1 light measurement', () => {
+    const measurements: Measurement[] = [{value: 12, createdAt: '2021-12-09 20:00:00'}];
+    const params = new HttpParams().set('limit', 1);
+
+		httpClientSpy.get.and.returnValue(of(measurements)
+      .pipe(map(data => {
+        return data.map((measurement: Measurement) => {
+          return measurement;
+        });
+      }))
+    );
+    measurementsService.getMeasurement('light', 1)
+      .subscribe((measurements: Measurement[]) => {
+        expect(measurements)
+          .withContext('to be true')
+          .toBeTruthy();
+
+        expect(measurements.length)
+          .withContext('expected to be greater than 0')
+          .toBeGreaterThan(0);
+    });
+
+
+    const req = httpTestingController.expectOne(testUrl + '/light/oneDay?limit=1');
+    expect(req.request.method).toBe('GET');
+    expect(req.request.params.get('limit')).toEqual(params.get('limit'));
+
+    req.flush(measurements);
+  });
+
+  it('should get 1 light measurement of specific day', () => {
+    const measurements: Measurement[] = [{value: 12, createdAt: '2021-12-09 20:00:00'}];
+    const date = '2021-12-09'
+    const params = new HttpParams().set('date', date);
+
+		httpClientSpy.get.and.returnValue(of(measurements)
+      .pipe(map(data => {
+        return data.map((measurement: Measurement) => {
+          return measurement;
+        });
+      }))
+    );
+    
+    measurementsService.getDateMeasurement('light', date)
+      .subscribe((measurements: Measurement[]) => {
+        expect(measurements)
+          .withContext('to be true')
+          .toBeTruthy();
+
+        expect(measurements.length)
+          .withContext('expected to be greater than 0')
+          .toBeGreaterThan(0);
+    });
+
+    const req = httpTestingController.expectOne(testUrl + '/light?date=2021-12-09');
+
+    expect(req.request.method).toBe('GET');
+    expect(req.request.params.get('date')).toEqual(params.get('date'));
+
+    req.flush(measurements)
+
   });
 });
