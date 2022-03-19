f6127c4b8d5b16bce0fefce7b32c171b5f6a3c0f
mattix10 <mateusz.salamon10@gmail.com>
2022-03-19 18:25:09 +0100

    add myAcoount tests

--- src/app/core/services/my-account.service.spec.ts
+++ src/app/core/services/my-account.service.spec.ts
@@ -1,16 +1,88 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../interfaces/User';
 import { MyAccountService } from './my-account.service';
 
-describe('MyAccountService', () => {
+xdescribe('MyAccountService', () => {
   let service: MyAccountService;
-
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let httpTestingController: HttpTestingController;
+  const testUrl = `${environment.API_URL}`;
+  let httpClient: HttpClient;
+  
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'put']);
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, HttpClientTestingModule],
+      providers: [MyAccountService,
+        {provide: HttpClient, useValue: httpClientSpy}]
+    });
     service = TestBed.inject(MyAccountService);
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  afterEach(() => {
+    httpTestingController.verify();
+  })
+
+  it('should create MyAccountService', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get user data', () => {
+    const user: User = {
+      email: 'test@test.com',
+      password: 'mypassword'
+    }
+
+    httpClientSpy.get.and.returnValue(of(user));
+
+    service.getUserData()
+      .subscribe((user: User) => {
+        expect(user)
+          .withContext('to be true')
+          .toBeTruthy();
+
+        expect(user)
+          .withContext('expected to be user')
+          .toEqual(user);
+    });
+
+    const req = httpTestingController.expectOne(testUrl + '/user');
+    expect(req.request.method).toBe('GET');
+
+    req.flush(user);
+  })
+
+  it('should return user after change password', () => {
+    const user: User = {
+      email: 'test@test.com',
+      password: 'mypassword'
+    }
+
+    const newPassword = 'newpassword'
+
+    httpClientSpy.put.and.returnValue(of(user));
+
+    service.editPassword(user.email,newPassword)
+      .subscribe((user: User) => {
+        expect(user)
+          .withContext('to be true')
+          .toBeTruthy();
+
+        expect(user)
+          .withContext('expected to be user')
+          .toEqual(user);
+    });
+
+    const req = httpTestingController.expectOne(testUrl + '/user');
+    expect(req.request.method).toBe('put');
+    expect(req.request.body).toEqual(user);
+    req.flush(user);
+  })
 });
